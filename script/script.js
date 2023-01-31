59f41e087b9282753977c56310163d2607eac0d7
Clelia-M <114869080+Clelia-M@users.noreply.github.com>
2023-01-31 21:06:47 +0000

    basic todayData funciton and pseudocode

--- script/script.js
+++ script/script.js
@@ -12,12 +12,29 @@ $("#search-form").submit(function (event) {
 function getWeather(city) {
     // Get weather data for a city for TODAY
     var todayUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city +
-    "&appid=" +
-    apiKey +
-    // units = metric used to have Celsius instead of Kelvin that is the Openweather standard
-    "&units=metric";
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&appid=" +
+        apiKey +
+        // units = metric used to have Celsius instead of Kelvin that is the Openweather standard
+        "&units=metric";
+
+    //jQuery to send GET request to OpenWeather API
+    // Data returned from the API to be passed in a function as parameter data
+    $.getJSON(todayUrl, function (todayData) {
+        // Declare var and assign the value of sata
+        var today = todayData;
+        // Create an URL for the weather icon, using the icon code from the API data stored in a var
+        var todayIcon =
+            "https://openweathermap.org/img/wn/" +
+            today.weather[0].icon +
+            "@2x.png";
+        // Create a string of HTML that will be used to display the weather data on the page
+        // Update the content of an HTML element with the ID "today" with the HTML string created
+    }
+
+
+    )
 }
 
 
