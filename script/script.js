aa5b1150bab07afb0eae79a7d99cfabad623231d
Clelia-M <114869080+Clelia-M@users.noreply.github.com>
2023-01-31 18:51:12 +0000

    initial pseudocode

--- script/script.js
+++ script/script.js
@@ -2,3 +2,8 @@
 var apiKey = "2e1ea90628f9fb67477f87ae9ccea177";
 
 // Form submit event
+// Function to get weather for TODAY
+
+// Get the gepgraphical ccordinates of the city
+
+// Function to get weather for the next 5 days
