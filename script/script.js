ba08f0abc3b08ebc6331f56fdaa4e9659d0edd5c
Clelia-M <114869080+Clelia-M@users.noreply.github.com>
2023-01-31 18:53:44 +0000

    function to submit city search

--- script/script.js
+++ script/script.js
@@ -2,6 +2,12 @@
 var apiKey = "2e1ea90628f9fb67477f87ae9ccea177";
 
 // Form submit event
+$("#search-form").submit(function (event) {
+    event.preventDefault();
+    var city = $("#search-input").val();
+    getWeather(city);
+});
+
 // Function to get weather for TODAY
 
 // Get the gepgraphical ccordinates of the city
