403109a66dc25a1295823d10a5ceaafa6ee5dcc0
Clelia-M <114869080+Clelia-M@users.noreply.github.com>
2023-01-31 21:22:19 +0000

    html linked to js and bug fixed

--- script/script.js
+++ script/script.js
@@ -19,10 +19,10 @@ function getWeather(city) {
         // units = metric used to have Celsius instead of Kelvin that is the Openweather standard
         "&units=metric";
 
-    //jQuery to send GET request to OpenWeather API
+    // jQuery to send GET request to OpenWeather API
     // Data returned from the API to be passed in a function as parameter data
     $.getJSON(todayUrl, function (todayData) {
-        // Declare var and assign the value of sata
+        // Declare var and assign the value of data
         var today = todayData;
         // Create an URL for the weather icon, using the icon code from the API data stored in a var
         var todayIcon =
@@ -32,11 +32,11 @@ function getWeather(city) {
         // Create a string of HTML that will be used to display the weather data on the page
         var todayHtml =
         // City name
-        "<h2>" + today.name + "-" +
+        "<h2>" + today.name + " - " +
         // Date of today
         moment().format("MMMM Do YYYY") + "</h2>" +
         // Weather icon
-        "<img src='" + todayIcon + "'>"
+        "<img src='" + todayIcon + "'>" +
         // Temperature
         "<p>Temperature: " + (today.main.temp).toFixed(2) + "°C</p>" +
         // Humidity
@@ -45,13 +45,11 @@ function getWeather(city) {
         "<p>Wind: " + today.wind.speed + "mph </p>";
 
         // Update the content of an HTML element with the ID "today" with the HTML string created
-    }
-
-
-    )
+        $("#today").html(todayHtml);
+    });
 }
 
 
-// Get the gepgraphical ccordinates of the city
+// Get the gepgraphical coordinates of the city
 
 // Function to get weather for the next 5 days
